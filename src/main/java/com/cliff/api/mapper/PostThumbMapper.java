8b30821cf0fbe84e0c562d50fdeaee841e9007f3
turkey <1281314239@qq.com>
2024-03-03 01:49:39 +0800

    初始化init

---
+++ src/main/java/com/cliff/api/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.cliff.api.mapper;
+
+import com.cliff.api.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * 
+ * 
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/cliff/api/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.cliff.api.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cliff.api.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * 
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
